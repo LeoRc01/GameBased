cbbb86758d086b428532fec914d01eb98fd0874d
Simone Biotto <simone.biotto@medas-solutions.it>
2022-11-01 18:53:55 +0100

    fixed MVVM pattern in PasswordReset fragment

--- app/src/main/java/com/cip/cipstudio/view/fragment/PasswordResetFragment.kt
+++ app/src/main/java/com/cip/cipstudio/view/fragment/PasswordResetFragment.kt
@@ -5,29 +5,42 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.cip.cipstudio.R
 import com.cip.cipstudio.databinding.FragmentPasswordResetBinding
 import com.cip.cipstudio.viewmodel.LoginViewModel
+import com.cip.cipstudio.viewmodel.PasswordResetViewModel
 
 class PasswordResetFragment : Fragment() {
 
-    private lateinit var loginViewModel: LoginViewModel
-    private lateinit var resetPasswordResetBinding: FragmentPasswordResetBinding
+    private lateinit var passwordResetViewModel: PasswordResetViewModel
+    private lateinit var passwordResetBinding: FragmentPasswordResetBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        resetPasswordResetBinding = FragmentPasswordResetBinding.inflate(inflater, container, false)
-        loginViewModel = LoginViewModel(requireContext())
+        passwordResetBinding = FragmentPasswordResetBinding.inflate(inflater, container, false)
+        passwordResetViewModel = PasswordResetViewModel(requireContext())
 
-        resetPasswordResetBinding.authViewModel = loginViewModel
+        passwordResetBinding.passwordResetViewModel = passwordResetViewModel
 
-        resetPasswordResetBinding.fPasswordResetIconBtnBack.setOnClickListener {
+        passwordResetBinding.fPasswordResetIconBtnBack.setOnClickListener {
             findNavController().navigate(R.id.action_passwordResetFragment_to_loginFragment)
         }
 
-        return resetPasswordResetBinding.root
+        passwordResetBinding.fPasswordResetBtnSendEmail.setOnClickListener {
+            passwordResetViewModel.resetPassword(
+                onSuccess = {
+                    Toast.makeText(requireContext(), "Da implementare", Toast.LENGTH_SHORT).show()
+                },
+                onFailure = {
+                    passwordResetBinding.fPasswordResetLayoutEmail.error = it.getErrorMessage(requireContext())
+                }
+            )
+        }
+
+        return passwordResetBinding.root
     }
 }
