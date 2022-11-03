aab529dede12e50c5fc0bda115a0d0669f1b20ce
Federico Pulcino <f.pulcino@campus.unimib.it>
2022-11-03 09:21:48 +0100

    Added navigation bar -- may not work

--- app/src/main/java/com/cip/cipstudio/view/MainActivity.kt
+++ app/src/main/java/com/cip/cipstudio/view/MainActivity.kt
@@ -2,13 +2,19 @@ package com.cip.cipstudio.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.cip.cipstudio.R
 import com.cip.cipstudio.repository.IGDBRepository
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var navController: NavController
     private lateinit var gameRepo : IGDBRepository
+    private val TAG = "MainActivity"
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,5 +22,14 @@ class MainActivity : AppCompatActivity() {
         gameRepo = IGDBRepository()
 
         supportActionBar!!.hide()
+
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fCVactivity_main) as NavHostFragment
+        navController = navHostFragment.navController
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+
+
+        NavigationUI.setupWithNavController(bottomNavigationView, navController)
     }
 }
