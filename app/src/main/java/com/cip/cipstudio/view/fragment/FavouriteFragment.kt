a49150d2a2bc651bba9f3fb40a45ba0e799fd4db
Simone Biotto <simone.biotto@medas-solutions.it>
2022-11-20 23:56:27 +0100

    add SwipeRefresh to Favourites

--- app/src/main/java/com/cip/cipstudio/view/fragment/FavouriteFragment.kt
+++ app/src/main/java/com/cip/cipstudio/view/fragment/FavouriteFragment.kt
@@ -2,6 +2,8 @@ package com.cip.cipstudio.view.fragment
 
 import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.provider.ContactsContract.Data
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -10,41 +12,62 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.RequiresApi
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.cip.cipstudio.R
+import com.cip.cipstudio.adapters.FavouriteGridViewAdapter
 import com.cip.cipstudio.databinding.FragmentFavouriteBinding
 import com.cip.cipstudio.databinding.FragmentGameDetailsBinding
+import com.cip.cipstudio.model.data.GameDetails
 import com.cip.cipstudio.repository.MyFirebaseRepository
 import com.cip.cipstudio.viewmodel.FavouriteViewModel
 import com.cip.cipstudio.viewmodel.GameDetailsViewModel
 
 class FavouriteFragment : Fragment() {
+    private val TAG = "FavouriteFragment"
 
     private lateinit var favouriteBinding: FragmentFavouriteBinding
     private lateinit var favouriteViewModel: FavouriteViewModel
 
-    private var myView : View? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        if(myView==null){
-            Log.i("NULL", "NULL VIEW")
-            favouriteBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_favourite, container, false)
+        Log.i("NULL", "NULL VIEW")
+        favouriteBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_favourite, container, false)
 
-            favouriteViewModel = FavouriteViewModel(favouriteBinding)
-            favouriteBinding.vm = favouriteViewModel
-            favouriteBinding.lifecycleOwner = this
-            myView = favouriteBinding.root
+        favouriteViewModel = FavouriteViewModel(favouriteBinding)
+
+        initializeFavourites()
+
+        favouriteBinding.vm = favouriteViewModel
+        favouriteBinding.lifecycleOwner = this
+
+        favouriteBinding.fFavouriteSrlRefresh.setOnRefreshListener {
+            Log.i(TAG, "Refreshing")
+            initializeFavourites()
+            Handler(Looper.getMainLooper())
+                .postDelayed( {
+                    favouriteBinding.fFavouriteSrlRefresh.isRefreshing = false
+                }, 2000)
         }
-        (myView!!.parent as ViewGroup?)?.removeView(myView)
 
-        return myView!!
-        //return favouriteBinding.root
+        return favouriteBinding.root
     }
 
     override fun onDestroy() {
         super.onDestroy()
         favouriteBinding.unbind()
+    }
+
+    private fun initializeFavourites(){
+        favouriteViewModel.initialize {
+            val gvAdapter = FavouriteGridViewAdapter(requireContext(),
+                it,
+                R.id.action_fav_to_gameDetailsFragment3,
+                favouriteBinding.root.findNavController())
+            favouriteBinding.gvFavoriteGames.adapter = gvAdapter
+        }
+
     }
 }
