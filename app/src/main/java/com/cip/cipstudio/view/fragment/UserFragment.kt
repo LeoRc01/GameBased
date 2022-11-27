849add78843877431ed44daf0325ae7e223e0c81
Federico Pulcino <f.pulcino@campus.unimib.it>
2022-11-27 11:19:47 +0100

    Implemented Feature ChangeLanguage + minor UI fix

--- app/src/main/java/com/cip/cipstudio/view/fragment/UserFragment.kt
+++ app/src/main/java/com/cip/cipstudio/view/fragment/UserFragment.kt
@@ -1,34 +1,32 @@
 package com.cip.cipstudio.view.fragment
 
+
 import android.app.Activity
 import android.content.Context.MODE_PRIVATE
 import android.content.Intent
 import android.content.SharedPreferences
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.cip.cipstudio.R
 import com.cip.cipstudio.databinding.FragmentUserBinding
-import com.cip.cipstudio.model.User.photoUrl
-import com.cip.cipstudio.model.User.username
 import com.cip.cipstudio.view.AuthActivity
 import com.cip.cipstudio.viewmodel.UserViewModel
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.UserProfileChangeRequest
-import com.google.firebase.auth.ktx.userProfileChangeRequest
 import com.google.firebase.storage.FirebaseStorage
-import com.google.firebase.storage.StorageReference
 import com.squareup.picasso.Picasso
-import io.grpc.Context.Storage
 import java.util.*
+
 
 class UserFragment : Fragment() {
     private val TAG = "UserFragment"
@@ -42,14 +40,16 @@ class UserFragment : Fragment() {
     private var selectedPhotoUri : Uri? = null
     private val filename = currentUser?.uid
     private val ref = FirebaseStorage.getInstance().getReference("/images/$filename")
+    private val systemLanguage: String = Locale.getDefault().language
 
+    @RequiresApi(Build.VERSION_CODES.P)
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        preferences = requireContext().getSharedPreferences(getString(R.string.setting_preferences), MODE_PRIVATE)
+        preferences = requireContext().getSharedPreferences(getString(com.cip.cipstudio.R.string.setting_preferences), MODE_PRIVATE)
 
-        userBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_user, container, false)
+        userBinding = DataBindingUtil.inflate(inflater, com.cip.cipstudio.R.layout.fragment_user, container, false)
         userViewModel = UserViewModel(userBinding)
 
 
@@ -71,27 +71,27 @@ class UserFragment : Fragment() {
         }
 
         userBinding.fUserTvChangeEmail.setOnClickListener {
-            findNavController().navigate(R.id.action_userFragment_to_changeEmailFragment)
+            findNavController().navigate(com.cip.cipstudio.R.id.action_userFragment_to_changeEmailFragment)
         }
 
         userBinding.fUserTvChangePassword.setOnClickListener {
-            findNavController().navigate(R.id.action_userFragment_to_changePasswordFragment)
+            findNavController().navigate(com.cip.cipstudio.R.id.action_userFragment_to_changePasswordFragment)
         }
         
         userBinding.fUserTvChangeUsername.setOnClickListener {
-            findNavController().navigate(R.id.action_userFragment_to_changeUsernameFragment)
+            findNavController().navigate(com.cip.cipstudio.R.id.action_userFragment_to_changeUsernameFragment)
         }
 
 
-        userBinding.fUserScDarkMode.isChecked = preferences.getBoolean(getString(R.string.dark_mode_settings), false)
+        userBinding.fUserScDarkMode.isChecked = preferences.getBoolean(getString(com.cip.cipstudio.R.string.dark_mode_settings), false)
         userBinding.fUserScDarkMode.setOnCheckedChangeListener { _, isChecked ->
             if (isChecked) {
                 userViewModel.setDarkMode(onSuccess = {
-                    preferences.edit().putBoolean(getString(R.string.dark_mode_settings), true).apply()
+                    preferences.edit().putBoolean(getString(com.cip.cipstudio.R.string.dark_mode_settings), true).apply()
                 })
             } else {
                 userViewModel.setLightMode(onSuccess = {
-                    preferences.edit().putBoolean(getString(R.string.dark_mode_settings), false).apply()
+                    preferences.edit().putBoolean(getString(com.cip.cipstudio.R.string.dark_mode_settings), false).apply()
                 })
             }
         }
@@ -102,6 +102,24 @@ class UserFragment : Fragment() {
             startActivityForResult(intent, 0)
         }
 
+        if (preferences.getString(getString(R.string.language_settings), systemLanguage) == "it") {
+            userBinding.fUserActvChangeLanguage.setText(getString(R.string.Italian), false)
+        } else {
+            userBinding.fUserActvChangeLanguage.setText(getString(R.string.English), false)
+        }
+
+        userBinding.fUserActvChangeLanguage.setOnItemClickListener { adapterView, view, i, l ->
+            val language = adapterView.getItemAtPosition(i).toString()
+            if (language == getString(R.string.Italian)) {
+                userViewModel.setItalianLanguage(onSuccess = {
+                    preferences.edit().putString(getString(com.cip.cipstudio.R.string.language_settings), "it").apply()
+                })
+            } else {
+                userViewModel.setEnglishLanguage(onSuccess = {
+                    preferences.edit().putString(getString(com.cip.cipstudio.R.string.language_settings), "en").apply()
+                })
+            }
+        }
 
         return userBinding.root
     }
