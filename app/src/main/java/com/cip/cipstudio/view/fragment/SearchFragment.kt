c7b815d282bd5c00a35f89e216e9436ea8c1263f
Roxas <davide.roxas@libero.it>
2022-11-30 13:16:07 +0100

    Search frontend and started backend
    Added search and searchitems layouts, started working on backend

--- app/src/main/java/com/cip/cipstudio/view/fragment/SearchFragment.kt
+++ app/src/main/java/com/cip/cipstudio/view/fragment/SearchFragment.kt
@@ -1,20 +1,30 @@
 package com.cip.cipstudio.view.fragment
 
+import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.SearchView
 import androidx.core.os.bundleOf
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.cip.cipstudio.R
+import com.cip.cipstudio.adapters.GamesRecyclerViewAdapter
 import com.cip.cipstudio.databinding.FragmentSearchBinding
 import com.cip.cipstudio.utils.ActionGameDetailsEnum
+import com.cip.cipstudio.utils.AuthTypeErrorEnum
+import com.cip.cipstudio.utils.GameTypeEnum
+import com.cip.cipstudio.view.MainActivity
 import com.cip.cipstudio.view.widgets.LoadingSpinner
 import com.cip.cipstudio.viewmodel.SearchViewModel
+import com.facebook.shimmer.ShimmerFrameLayout
 import kotlinx.coroutines.*
 
 class SearchFragment : Fragment() {
@@ -36,23 +46,128 @@ class SearchFragment : Fragment() {
         searchBinding.vm = searchViewModel
         searchBinding.lifecycleOwner = this
 
+        // roba che fa avviata la gui
+
         sharedPreferences = searchBinding.root.context.getSharedPreferences(
             getString(R.string.setting_preferences),
             AppCompatActivity.MODE_PRIVATE)
 
-        searchBinding.fSearchButton.setOnClickListener {
-            search()
-        }
-
-        searchBinding.fSearchBtnTestGuest.setOnClickListener {
-            if (sharedPreferences.contains(getString(R.string.guest_settings)))
-                sharedPreferences.edit().remove(getString(R.string.guest_settings)).apply()
-        }
-
+        initializeSearchView()
         return searchBinding.root
     }
 
+
+    private fun initializeSearchView() {
+
+        searchBinding.fSearchSearchBox.setOnQueryTextListener(object :
+            SearchView.OnQueryTextListener {
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                return false
+            }
+
+            override fun onQueryTextSubmit(query: String): Boolean {
+
+                // roba che fa avviata la ricerca
+
+                LoadingSpinner.showLoadingDialog(requireContext())
+                initializeSearchResultsList()
+                LoadingSpinner.dismiss()
+
+                /*searchViewModel.search(onSuccess = {
+
+                },
+                    onFailure = {
+
+                    }
+                )*/
+
+                return false
+            }
+
+        })
+
+    }
+
+    private fun initializeSearchResultsList(refresh: Boolean = false) {
+        // SearchResults
+        initializeRecyclerView(
+            searchBinding.fSearchResults,
+            searchBinding.fSearchShimmerLayoutResults,
+            refresh
+        )
+    }
+
+    // senza shimmer, non posso usare la stessa funzione, da vedere dopo
+    /*private fun initializeSearchHistoryList() {
+        // SearchHistory
+        initializeRecyclerView(
+            searchBinding.fSearchHistoryList,
+        )
+    }*/
+
+    private fun initializeRecyclerView(
+        recyclerView: RecyclerView,
+        shimmerLayout: ShimmerFrameLayout,
+        refresh: Boolean
+    ) {
+        shimmerLayout.startShimmer()
+        val linearLayoutManager = LinearLayoutManager(requireContext())
+        linearLayoutManager.orientation = LinearLayoutManager.HORIZONTAL
+
+        val adapter = GamesRecyclerViewAdapter(
+            ArrayList(),
+            ActionGameDetailsEnum.MAIN_PAGE
+        )
+
+        recyclerView.layoutManager = linearLayoutManager
+        recyclerView.adapter = adapter
+        recyclerView.itemAnimator = null
+        recyclerView.setItemViewCacheSize(50)
+
+        searchViewModel.initializeRecyclerView(refresh) {
+            adapter.importItems(it)
+            shimmerLayout.stopShimmer()
+            shimmerLayout.visibility = View.GONE
+        }
+    }
+
+
+        /*
+
+        searchBinding.fLoginBtnLogin.setOnClickListener {
+            LoadingSpinner.showLoadingDialog(requireContext())
+            loginBinding.fLoginLayoutEmail.error = ""
+            loginBinding.fLoginLayoutPwd.error = ""
+            loginViewModel
+                .login(onSuccess = {
+                    LoadingSpinner.dismiss()
+                    val intent = Intent(requireContext(), MainActivity::class.java)
+                    startActivity(intent)
+                    requireActivity().finish()
+                },
+                    onFailure = {
+                        LoadingSpinner.dismiss()
+                        when(it.getErrorType()){
+                            AuthTypeErrorEnum.EMAIL -> {
+                                loginBinding.fLoginLayoutEmail.error = getString(it.getErrorId())
+                            }
+                            AuthTypeErrorEnum.PASSWORD -> {
+                                loginBinding.fLoginLayoutPwd.error = getString(it.getErrorId())
+                            }
+                            AuthTypeErrorEnum.UNKNOWN -> {
+                                Toast.makeText(context, getString(it.getErrorId()), Toast.LENGTH_SHORT).show()
+                            }
+                            else -> {
+                                Toast.makeText(context, getString(R.string.internal_error), Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                    })
+        }
+    }
+
     private fun search () {
+
         LoadingSpinner.showLoadingDialog(requireContext())
 
         getGames {
@@ -76,4 +191,6 @@ class SearchFragment : Fragment() {
 
         onSuccess.invoke()
     }
+
+         */
 }
--- app/src/main/java/com/cip/cipstudio/viewmodel/SearchViewModel.kt
+++ app/src/main/java/com/cip/cipstudio/viewmodel/SearchViewModel.kt
@@ -7,17 +7,39 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.cip.cipstudio.adapters.GamesBigRecyclerViewAdapter
 import com.cip.cipstudio.dataSource.repository.HistoryRepository
+import com.cip.cipstudio.dataSource.repository.IGDBRepository
 import com.cip.cipstudio.databinding.FragmentSearchBinding
 import com.cip.cipstudio.model.data.GameDetails
 import com.cip.cipstudio.dataSource.repository.IGDBRepositoryImpl.IGDBRepositoryRemote
 import com.cip.cipstudio.dataSource.repository.historyRepositoryImpl.HistoryRepositoryLocal
 import com.cip.cipstudio.model.User
+import com.cip.cipstudio.utils.GameTypeEnum
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 
 class SearchViewModel(val binding : FragmentSearchBinding) : ViewModel(){
+
+    var searchtext : MutableLiveData<String> = MutableLiveData()
+
+    private val gameRepository : IGDBRepository = IGDBRepositoryRemote
+
+    fun initializeRecyclerView(refresh : Boolean,
+                               updateUI : (List<GameDetails>)->Unit) {
+
+        var games :List<GameDetails>
+        viewModelScope.launch(Dispatchers.Main) {
+            games = withContext(Dispatchers.IO) {
+                gameRepository.searchGames(searchtext, refresh)
+            }
+            updateUI.invoke(games)
+        }
+
+
+    }
+
+    /*
 
     val isPageLoading : MutableLiveData<Boolean> by lazy {
         MutableLiveData<Boolean>(true)
@@ -57,5 +79,7 @@ class SearchViewModel(val binding : FragmentSearchBinding) : ViewModel(){
         binding.fSearchRvLastViewedGames.itemAnimator = null
         binding.fSearchRvLastViewedGames.adapter = lastViewGamesRecyclerViewAdapter
     }
+
+    */
 
 }
