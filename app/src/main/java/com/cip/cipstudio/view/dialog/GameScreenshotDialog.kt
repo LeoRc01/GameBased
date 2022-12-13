c0630916123ed2eae46ae8ce43fd27383b364bd0
Leonardo Valente <agama1257@gmail.com>
2022-12-13 16:14:33 +0100

    fixed screenshot dialog

--- app/src/main/java/com/cip/cipstudio/view/dialog/GameScreenshotDialog.kt
+++ app/src/main/java/com/cip/cipstudio/view/dialog/GameScreenshotDialog.kt
@@ -1,6 +1,7 @@
 package com.cip.cipstudio.view.dialog
 
 import android.content.res.Configuration
+import android.content.res.Resources.Theme
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -11,6 +12,7 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.LinearLayout
 import androidx.browser.trusted.ScreenOrientation
+import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.DialogFragment
 import androidx.lifecycle.MutableLiveData
 import com.cip.cipstudio.R
@@ -29,12 +31,15 @@ class GameScreenshotDialog() : DialogFragment() {
         Picasso.get().load(imageUrl).into(imageView)
         dialog!!.setContentView(view)
 
-        dialog!!.window!!.setLayout(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog!!.window!!.setLayout(
+            if(resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) 1800 else ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog!!.window!!.setBackgroundDrawable(ResourcesCompat.getDrawable(resources, R.color.transparent, null))
 
         imageView.layoutParams =
             LinearLayout.LayoutParams(
                 LinearLayout.LayoutParams.MATCH_PARENT,
-                if(resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) LinearLayout.LayoutParams.MATCH_PARENT else 600,
+                if(resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) LinearLayout.LayoutParams.MATCH_PARENT else 700,
             )
         return super.onCreateView(inflater, container, savedInstanceState)
     }
